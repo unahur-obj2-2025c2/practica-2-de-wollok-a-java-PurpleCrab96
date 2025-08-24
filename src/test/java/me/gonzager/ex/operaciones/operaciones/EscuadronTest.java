b341140780fed5caf4762a69fac484418266c16e
PurpleCrab96 <gastonexequielfernandez@gmail.com>
2025-08-24 12:45:02 -0300

    Agregar pruebas unitarias para las clases Dron y Mision, y crear la clase EscuadronTest

---
+++ src/test/java/me/gonzager/ex/operaciones/operaciones/EscuadronTest.java
@@ -0,0 +1,63 @@
+package me.gonzager.ex.operaciones.operaciones;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertFalse;
+import static org.junit.jupiter.api.Assertions.assertThrows;
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import me.gonzager.ex.operaciones.drones.Dron;
+import me.gonzager.ex.operaciones.drones.DronComercial;
+import me.gonzager.ex.operaciones.drones.DronSeguridad;
+import me.gonzager.ex.operaciones.misiones.MisionTransporte;
+import me.gonzager.ex.operaciones.misiones.MisionVigilancia;
+
+public class EscuadronTest {
+    Escuadron escuadron = new Escuadron();
+    private Dron dronSeguridad = new DronSeguridad(8, 52);
+    private Dron dronComercial = new DronComercial(1, 10);
+
+    private MisionVigilancia misionVigilancia = new MisionVigilancia();
+    private MisionTransporte misionTransporte = new MisionTransporte();
+
+    @BeforeEach
+    private void init() {
+        dronComercial.setMision(misionTransporte);
+        dronSeguridad.setMision(misionVigilancia);
+        escuadron.agregarDron(dronComercial);
+        escuadron.agregarDron(dronSeguridad);
+
+    }
+
+    @Test()
+    void testNoSePuedeAgregarDronAlEscuadron() {
+        Escuadron.setCantidadMaximaDrones(0);
+        assertThrows(IllegalArgumentException.class, () -> {
+            escuadron.agregarDron(new DronComercial(0, 0));
+        });
+    }
+
+    @Test()
+    void noPuedeOperarZona() {
+        var zona = new Zona(100);
+        assertFalse(escuadron.puedeOperar(zona));
+    }
+
+    @Test()
+    void puedeOperarZona() {
+        var zona = new Zona(5);
+        assertTrue(escuadron.puedeOperar(zona));
+    }
+
+    @Test()
+    void operarZona() {
+        var zona = new Zona(5);
+        escuadron.operarZona(zona);
+        assertEquals(1, zona.getCantidadOperacionesRecibidas());
+        assertEquals(6, dronSeguridad.getAutonomia());
+        assertEquals(0, dronComercial.getAutonomia());
+    }
+
+}

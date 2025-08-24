9c01396ee1bf010b0eafd4aaf26e9844aea520b3
PurpleCrab96 <gastonexequielfernandez@gmail.com>
2025-08-24 15:13:04 -0300

    Eliminar importaciones innecesarias en MisionTest y EscuadronTest para mejorar la claridad del código

--- src/test/java/me/gonzager/ex/operaciones/misiones/MisionTest.java
+++ src/test/java/me/gonzager/ex/operaciones/misiones/MisionTest.java
@@ -2,7 +2,6 @@ package me.gonzager.ex.operaciones.misiones;
 
 import static org.junit.jupiter.api.Assertions.assertFalse;
 import static org.junit.jupiter.api.Assertions.assertTrue;
-
 import org.junit.jupiter.api.Test;
 
 import me.gonzager.ex.operaciones.accesorios.Sensor;
